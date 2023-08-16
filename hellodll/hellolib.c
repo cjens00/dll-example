481ada3c9c3b8366594669e2d619e8dfaaa8ed19
cam <cj00>
2023-08-15 19:54:10 -0600

    Added comments to hellolib.h to explain the import/export process.

--- hellodll/hellolib.c
+++ hellodll/hellolib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "hellolib.h"
 
+// The implementation of the function to be exported
 void print_hello_world() {
     printf("Hello, world! From hellolib (DLL).");
 }
--- hellodll/hellolib.h
+++ hellodll/hellolib.h
@@ -1,25 +1,37 @@
 #ifndef HELLOLIB_H
 #define HELLOLIB_H
 
-// ================================
+// ===========================================================================
+// If using a C++ compiler, retain C-style interfaces for exported functions. 
+// This improves DLL import compatibility with other languages.
+// ===========================================================================
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-// ================================
 
+// ===========================================================================
+// When using Visual Studio or CMake, the default behavior is to define
+// a symbol of format <PROJECT-NAME_EXPORTS> when building a DLL. We check
+// to see if the DLL is being built, in which case we export. If not, we 
+// would like to tell the compiler that these are imported functions instead. 
+// ===========================================================================
 #ifdef hellodll_EXPORTS
 #define HELLOLIB_API __declspec(dllexport)
 #else
 #define HELLOLIB_API __declspec(dllimport)
 #endif
 
+// The function we are going to both export into the DLL 
+// and import into the client application.
 HELLOLIB_API void print_hello_world();
 
-// ================================
+// ===========================================================================
+// Here we are simply closing our C-style scope (if using C++ compiler).
+// ===========================================================================
 #ifdef __cplusplus
 }
 #endif
-// ================================
+// ===========================================================================
 
 #endif
